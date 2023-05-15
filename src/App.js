9bf87bab546377e2c8a843484b9852b62c201ba1
timothywilkins <tim.f.wilkins@gmail.com>
2023-05-14 22:36:18 -0500

    create basic working app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,125 @@
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import DeskSvg from './components/DeskSvg'
+import postits from './postits.png'
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [input, setInput] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+  const [time, setTime] = useState(0);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSubmit = (text) => {
+    const task = tasks.push(input)
+    setInput('')
+    tasks.concat(task)
+    handleCloseModal();
+  };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value)
+  };
+
+  const listTasks = tasks.map((task, i) =>
+    <div className='post-it' key={i}>
+      <div>{task}</div>
+    </div>
+  )
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive && isPaused === false) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, isPaused]);
+
+  const handleStart = () => {
+    if (isActive === false) {
+      setIsActive(true);
+      setIsPaused(false);
+    }
+    else {
+      setIsPaused(!isPaused);
+    }
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(0);
+  };
+
   return (
     <div className="App">
-     <div>React Task App</div>
+
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <div className="modal-content">
+              <h2>Enter a short line of text</h2>
+              <input type="text" onChange={handleInputChange} maxLength={20} />
+              <div className="modal-buttons">
+                <button onClick={handleCloseModal}>Cancel</button>
+                <button onClick={handleSubmit}>Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="tack-board-container" style={{ display: "flex" }}>
+        {tasks &&
+          listTasks
+        }
+      </div>
+
+      <div className="desk-container">
+        <div className="desk-items">
+
+          <div className="timer">
+            <div className="timer-display">
+              <span className="digits">
+                {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+              </span>
+              <span className="digits">
+                {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
+              </span>
+              <span className="digits mili-sec">
+                {("0" + ((time / 10) % 100)).slice(-2)}
+              </span>
+            </div>
+            <div className="button-start-resume">
+              <div className="button" onClick={handleStart}></div>
+            </div>
+            <div className="button-reset">
+              <div className="button" onClick={handleReset}></div>
+            </div>
+          </div>
+          <div className='postits' onClick={handleOpenModal}>
+            <img src={postits} alt="stack of post it notes" />
+          </div>
+        </div>
+        <div>
+          <DeskSvg />
+        </div>
+        {/* <div className="corner-top-both-bevel"></div> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/DeskSvg.js
@@ -0,0 +1,31 @@
+import * as React from "react"
+const DeskSvg = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    style={{
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      strokeLinejoin: "round",
+      strokeMiterlimit: 2,
+    }}
+    viewBox="0 0 1356 121"
+    {...props}
+  >
+    <path
+      d="M1377.94 485.541H260.897L141.847 540H1496.99l-119.05-54.459Z"
+      style={{
+        fill: "#ff6b00",
+      }}
+      transform="matrix(1 0 0 1.62791 -141.846 -790.415)"
+    />
+    <path
+      d="M372.348 628.654h1327.28v51.926H372.348z"
+      style={{
+        fill: "#dd5d00",
+      }}
+      transform="matrix(1.02099 0 0 .60976 -380.164 -294.671)"
+    />
+  </svg>
+)
+export default DeskSvg
